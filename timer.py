edaee90ffb5b72a23fddcd7b5d0c7da046ec1170
peteseta <petesetabandhu@gmail.com>
2023-08-05 16:12:58 +0700

    added docstrings

--- timer.py
+++ timer.py
@@ -11,8 +11,20 @@ class Timer:
     def __init__(
         self, parent, callback, subjects_with_duration, duration, section_id
     ) -> None:
+        """
+        Initializes the UI component for one timer
+        (one timer for one duration; one timer can have multiple subjects)
+        
+        Args:
+            parent (TimerPage): parent tkinter frame
+            callback (function): TimerPage.finish()
+            subjects_with_duration (list): list of subjects with the same duration
+            duration (datetime.timedelta): duration of the timer
+            section_id (list): IDs for each section in the timer
+            # TODO: use unique id to allow changing details after starting timer
+        """
         self.frame = ttk.Frame(parent, padding=10)
-        self.frame.grid_rowconfigure(2, weight=1)
+        self.frame.grid_rowconfigure(2, weight=1)  # expand Info to bottom
 
         self.callback = callback
         self.duration = duration
@@ -30,6 +42,10 @@ class Timer:
         self.progress_bar.update(datetime.timedelta(0), duration, duration)
 
     def start_timer(self):
+        """
+        Calculates the start time, end time, 5 and 30min warnings if applicable
+        Adds this information in an info frame (Info class)
+        """
         self.start_time = datetime.datetime.now()
         self.end_time = self.start_time + self.duration
         if self.duration > datetime.timedelta(minutes=30):
@@ -55,6 +71,11 @@ class Timer:
         self.update_loop()
 
     def update_loop(self):
+        """
+        Calculates the elapsed and remaining time
+        If the timer is over, call the callback
+        Calls itself to update every second
+        """
         self.elapsed = datetime.datetime.now() - self.start_time
         self.remaining = self.end_time - datetime.datetime.now()
 
@@ -69,6 +90,12 @@ class Timer:
 
 class ProgressBar:
     def __init__(self, parent) -> None:
+        """
+        Initializes the UI for the progressbar and elapsed/remaining text
+
+        Args:
+            parent (ttk.Frame): parent Timer frame
+        """
         self.canvas = ttk.Canvas(parent, height=120, width=440)
         self.canvas.grid(row=0)
 
@@ -101,6 +128,14 @@ class ProgressBar:
         self.progressbar_value.set(50)
 
     def update(self, elapsed, remaining, duration):
+        """
+        Updates the elapsed/remaining text and the progressbar
+
+        Args:
+            elapsed (datetime.timedelta): elapsed time
+            remaining (datetime.timedelta): remaining time
+            duration (datetime.timedelta): total duration of the section
+        """
         if elapsed.total_seconds() < 3600:
             elapsed_text = str(elapsed)[2:7]  # format as MM:SS
         else:
@@ -121,20 +156,37 @@ class ProgressBar:
 
 class SubjectList:
     def __init__(self, parent, subjects) -> None:
+        """
+        Initializes the UI for the list of subject names for this timer
+        Creates a SubjectLabel element for each subject/section in the timer
+
+        Args:
+            parent (ttk.Frame): parent Timer frame
+            subjects (list): list of Subject objects in the timer
+        """
         self.frame = ttk.Frame(parent)
         self.frame.grid(row=1, sticky="nw")
 
         self.labels = []
 
         for index, subject in enumerate(subjects):
-            self.labels.append(
-                SubjectLabel(self.frame, subject.name, subject.sections[0].name)
-            )
-            self.labels[index].frame.grid(row=index, sticky="w")
+            if subject.sections[index].section_in_progress:
+                self.labels.append(
+                    SubjectLabel(self.frame, subject.name, subject.sections[index].name)
+                )
+                self.labels[index].frame.grid(row=index, sticky="w")
 
 
 class SubjectLabel:
     def __init__(self, parent, subject_name, section_name) -> None:
+        """
+        Initializes the UI for a single subject's label
+
+        Args:
+            parent (ttk.Frame): parent SubjectList frame
+            subject_name (str): name of the subject
+            section_name (str): name of the section
+        """
         self.frame = ttk.Frame(parent, width=440)
         ttk.Label(
             self.frame,
@@ -160,6 +212,17 @@ class Info:
     def __init__(
         self, parent, duration, start_time, end_time, thirty_min, five_min
     ) -> None:
+        """
+        Initializes the UI for the info section of the timer
+
+        Args:
+            parent (ttk.Frame): parent Timer frame
+            duration (datetime.timedelta): total duration of the timer
+            start_time (datetime.datetime): start time of the timer
+            end_time (datetime.datetime): end time of the timer
+            thirty_min (datetime.datetime): time of the 30-minute mark
+            five_min (datetime.datetime): time of the 5-minute mark
+        """
         self.frame = ttk.Frame(parent, width=440)
         self.frame.grid(row=2, sticky="sw")
         self.frame.grid_rowconfigure(2, weight=1)
